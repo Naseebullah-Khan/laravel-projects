df3e20c166d0162fbc5d3c8498bdb1557b543181
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-13 20:43:28 +0430

    Employer's Migration, model, policy, controller , factory, and seeder are added.

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,16 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Mr.Khan</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Laravel Developer
+        </h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From 60,000AFG</p>
+    </div>
+    <div>
+        <x-tag href="#" size="small">Tag</x-tag>
+        <x-tag href="#" size="small">Tag</x-tag>
+        <x-tag href="#" size="small">Tag</x-tag>
+    </div>
+</x-panel>
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,15 @@
+<x-panel class="flex flex-col text-center">
+    <div class="self-start text-sm">Mr.Khan</div>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Laravel Developer</h3>
+        <p class="text-sm mt-4">Full Time - From 60,000AFG</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag href="#" size="small">Frontend</x-tag>
+            <x-tag href="#" size="small">Backend</x-tag>
+            <x-tag href="#" size="small">Manager</x-tag>
+        </div>
+        <x-employer-logo :width="42" />
+    </div>
+</x-panel>
