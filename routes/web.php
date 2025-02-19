382208587987e491b213e3d29fd8e6f2d197820d
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-19 19:20:53 +0430

    Add type hints to route closures for improved clarity

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Contracts\View\View;
 
 # Routes: A route act as a mapping between a specific URL and the cross-bonding code that should be executed in response to the user request.
 
-Route::get('/', function () {
+Route::get('/', function (): View {
     return view('welcome');
     // return "Hello, World!";
 });
 
-Route::get("/about", function () {
+Route::get("/about", function (): string {
     return "This is About Page.";
 });
 
