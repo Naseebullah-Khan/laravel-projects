ea17be16b49d916108f8c5cf474f09944b8f3468
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-20 20:31:23 +0430

    Extending Blade

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,9 @@ use Illuminate\Contracts\View\View;
 # Routes: A route act as a mapping between a specific URL and the cross-bonding code that should be executed in response to the user request.
 
 Route::get('/', function (): View {
-    $title = "This is Home Page!";
-    return view('welcome', ["title" => $title]);
-    // return "Hello, World!";
+    return view('welcome');
 });
 
-Route::get("/about", function (): View {
-    $books = ["Steal like a artist", "Story book", "48 Laws of Power"];
-    return view("about.index", ["title" => "This is About page!", "books" => $books]);
-});
-
-Route::get("/contact", function (): View {
-    $title = "This is Contact page!";
-    return view("contact.index", ["title" => $title]);
+Route::get("/about", function () {
+    return view("about.index");
 });
