5dbc0670c0c6cc3f4ed77693e659f8fbcf886597
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-21 00:25:35 +0430

    Resource Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SingleActionController;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,12 @@ Route::get('/', [HomeController::class, "index"]);
 Route::get("/about", [HomeController::class, "about"]);
 
 Route::get("/contact", SingleActionController::class);
+
+# Blog
+# Create
+# Read
+# Update
+# Delete
+# (CRUD)
+
+Route::resource("/blog", BlogController::class);
