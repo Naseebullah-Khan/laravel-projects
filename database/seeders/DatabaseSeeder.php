8a4b0c27c16a7b83e691f92eefbc3ca9d4abe7f3
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-21 22:48:35 +0430

    Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\MyBlog;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        MyBlog::factory(10)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
@@ -23,3 +24,4 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
     }
 }
+
