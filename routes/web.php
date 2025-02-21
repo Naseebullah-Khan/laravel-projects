87cf7c2ad94568a2a129141adcd75a8fb71ef1fa
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-21 15:47:00 +0430

    Model

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SingleActionController;
+use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 # Routes: A route act as a mapping between a specific URL and the cross-bonding code that should be executed in response to the user request.
@@ -20,4 +21,9 @@ Route::get("/contact", SingleActionController::class);
 # Delete
 # (CRUD)
 
-Route::resource("/blog", BlogController::class);
+// Route::resource("/blog", BlogController::class);
+
+Route::get("/blog", function (): string {
+    $blogs = Blog::all(); # SELECT * FROM blogs
+    dd($blogs);
+});
