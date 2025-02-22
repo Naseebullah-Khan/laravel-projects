8a4b53de033eed33f532aeb3365bbd54db009502
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-22 20:04:52 +0430

    Creating New Data Using Query Builder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(): View
     {
+        DB::table("users")->insert([
+            [
+                "name" => "Mahmood",
+                "email" => "mahmood@gmail.com",
+                "password" => "123456789",
+            ],
+            [
+                "name" => "Zafar",
+                "email" => "zafar@gmail.com",
+                "password" => "12345678",
+            ]
+        ]);
         return view("welcome");
     }
 
