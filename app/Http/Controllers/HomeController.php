b1cd13b99f7009cf96fea06b40a9c95678456210
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-22 21:10:25 +0430

    Updating Data Using Query Builder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,19 +7,12 @@ use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function index(): object|null
+    public function index(): View
     {
-        $users = DB::table("users")->get(); # All Data
-        // return $users;
-
-        $user1 = DB::table("users")->find(1, ["name", "email"]); # You can only get single data by id
-        // return $user1;
-        # or
-        $user2 = DB::table("users")->where("email", "nk0784494104@gmail.com")->first(); # You can get Single Data by any column
-        // return $user2;
-
-        $users1 = DB::table("users")->where("id", ">=", 3)->get();
-        return $users1;
+        DB::table("users")->where("email", "nk0784494104@gmail.com")->update([
+            "email" => "naseebullah_hoshmand@hotmail.com",
+        ]); # You can get Single Data by any column
+        return view("welcome");
     }
 
     public function about(): View
