9f990fe5075dd23485329e516b38a924a7806966
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-22 21:50:33 +0430

    Retrieving a List of Column Values Using Query Builder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function index(): View
+    public function index(): Collection
     {
-        DB::table("users")->where("email", "mahmood@gmail.com")->delete();
-        return view("welcome");
+        # get data from the database using get method (returns data in the form of an collection)
+        // $blogs = DB::table("blogs")->get(["title"]);
+        // return $blogs;
+
+        # get data from the database using select method (returns data in the form of an collection)
+        // $blogs = DB::table("blogs")->select("description")->get();
+        // return $blogs;
+
+        # get data from the database using select method (returns data in the form of an collection and converts it to array)
+        // $blogs = DB::table("blogs")->select("description")->get()->toArray();
+        // dd($blogs);
+
+        # get data from the database using pluck method (returns data in the form of an array) prefer way
+        # you have to pass a column to key that has unique values
+        $blogs = DB::table("blogs")->pluck("description", "id");
+        return $blogs;
     }
 
     public function about(): View
