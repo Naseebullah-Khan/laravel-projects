17289fede1cfa3a227439321a52be8ca05c27cb6
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-23 22:08:20 +0430

    Conditional Clause Using in Eloquent ORM and Query Builder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,38 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\Product;
 use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
-    public function index(): View
+    public function index()
     {
-        # when you want create one user you can use create method.
-        // User::create([
-        //     "name" => "TestName1",
-        //     "email" => "test1@email.com",
-        //     "password" => "1234",
-        //     "email_verified_at" => now(),
-        // ]);
+        // $product = Product::where("id", 2)->where("price", "<=", 26.62)->first();
+        # if both of the operator is equal then you can use this approach.
+        // $product = Product::where(["id" => 2, "price" => 26.62])->first();
 
-        # when you want to create one or more than one user then you can use insert method and it does not care about the model and interact directly with database thats why the casting in model does not work on password.
-        User::insert([
-            [
-                "name" => "TestName4",
-                "email" => "test4@email.com",
-                "password" => "1234",
-                "email_verified_at" => now(),
-            ],
-            [
-                "name" => "TestName5",
-                "email" => "test5@email.com",
-                "password" => "1234",
-                "email_verified_at" => now(),
-            ],
-        ]);
+        # if you want to search for something
+        // $products = Product::where("name", "LIKE", "%Assumenda%")->get();
 
-        return view("welcome");
+        # the only way that you dont get any result is when both of them fails.
+        // $products = Product::where("name", "LIKE", "%Assumenda%")->orWhere("description", "LIKE", "%cupiditate%")->get();
+
+        # we can also use whereIn method for searching multiple values but you have to search for the entire value.
+        // $products = Product::whereIn("name", ["Assumenda", "Quisquam quas ratione ut voluptatem veritatis.", "Nobis", "Voluptas", "Naseeb"])->get();
+
+        # if you want to search for products in specific range of price for example then you use whereBetween method.
+        $products = Product::whereBetween("price", [200, 400])->get();
+        return $products;
     }
 
     public function about(): View
