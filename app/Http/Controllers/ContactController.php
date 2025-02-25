38c58e3020071cd5c13c0f8bd7fa43503d5a2ae0
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-25 21:06:06 +0430

    Custom Validation Message

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,6 +18,11 @@ class ContactController extends Controller
         $request->validate([
             "name" => ["required", "max:20", "min:2"],
             "email" => ["required", "email"],
+        ], [
+            "name.required" => "Hey please fill the name field",
+            "name.max" => "The max length of name have to be 20",
+            "name.min" => "The min length of name have to be 2",
+            "email.required" => "Hey email is required",
         ]);
     }
 }
