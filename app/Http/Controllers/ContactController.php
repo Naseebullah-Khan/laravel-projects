f6ecd1bac99b02eb92037637d6ea5e520e479143
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-25 21:10:45 +0430

    Custom Request Classes

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactStoreRequest;
 use Illuminate\Contracts\View\View;
 
 
@@ -13,16 +13,8 @@ class ContactController extends Controller
         return view("contact.index");
     }
 
-    public function store(Request $request)
+    public function store(ContactStoreRequest $request)
     {
-        $request->validate([
-            "name" => ["required", "max:20", "min:2"],
-            "email" => ["required", "email"],
-        ], [
-            "name.required" => "Hey please fill the name field",
-            "name.max" => "The max length of name have to be 20",
-            "name.min" => "The min length of name have to be 2",
-            "email.required" => "Hey email is required",
-        ]);
+
     }
 }
