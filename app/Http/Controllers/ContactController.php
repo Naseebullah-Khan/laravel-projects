d3b7bf185460b59d85747359520ad7432881b289
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-02-25 19:23:15 +0430

    Accessing Form Contents from Method

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 
+
 class ContactController extends Controller
 {
     public function index(): View
@@ -12,8 +13,11 @@ class ContactController extends Controller
         return view("contact.index");
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        dd("working");
+        // dd(request()->all()); # using request() helper function to get the request data
+        // dd($request->all()); # using Request class to get the request data and this is the recommended way
+        // dd($request->name); # getting the name field from the request data
+        dd($request->input("email")); # getting the name field from the request data
     }
 }
