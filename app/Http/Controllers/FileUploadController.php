79888af40ffb7bd3fcbdc68966d19dbb01bb303a
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-03-02 15:25:10 +0430

    Change File Name Before Save

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -6,6 +6,7 @@ use App\Models\File;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Storage;
+use Str;
 
 class FileUploadController extends Controller
 {
@@ -17,24 +18,15 @@ class FileUploadController extends Controller
 
     public function store(Request $request): void
     {
-        # Two ways to store files
-        # in order to access the files in storage/public folder you have to link the root public folder to storage/public folder by using this command php artisan storage:link
-        # but there is a drawback for this because shared hosting server does not allow this command to be run. so by using this command php artisan storage:unlink we can unlink the root public folder from storage/public folder
+        $file = $request->file("file");
+        $customName = "laravel_basic_" . Str::uuid();
+        $extension = $file->getClientOriginalExtension(); // get file extension without dot like: image.phg => png not .png
+        $fileNewName = $customName . "." . $extension;
 
-        # 1. Storage Facade (Longer Version)
-        #    disk() -> where to store file
-        #    put() -> rootPath and file name
-        // $file = Storage::disk("local")->put("/", $request->file("file"));
-
-        # 2. Short Version
-        // $file = $request->file("file")->store("/", "local"); # this should be stored locally and can not be accessed by public
-        $file = $request->file("file")->store("/", "custom_disk_public"); # this should be stored locally and can be accessed by public if we link it with root public folder of laravel project
+        $path = $file->storeAs("/", $fileNewName, "custom_disk_public");
 
         $fileStore = new File();
-        # if you want to store file name
-        // $fileStore->file_path = $file;
-        # if you want to store file path
-        $fileStore->file_path = "/uploads/" . $file;
+        $fileStore->file_path = "/uploads/" . $path;
         $fileStore->save();
 
         dd("stored");
