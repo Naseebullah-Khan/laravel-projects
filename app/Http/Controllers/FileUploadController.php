96acd3e44c0b95e43d1f9e05685b2978ab3a422f
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-03-02 15:48:50 +0430

    Remove File From Storage

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -7,11 +7,24 @@ use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Storage;
 use Str;
+# main use case -> for manipulating the file
+use Illuminate\Support\Facades\File as FileFacades; # but we are gonna use it for deleting the file
 
 class FileUploadController extends Controller
 {
     public function index(): View
     {
+        # in order to delete the file first you have to get the path to the file then using that path delete the file in local storage then delete the path from the database
+        # do not delete the path before deleting the file
+
+        # Deleting file procedure
+        # 1. get the path
+        $file = File::where("id", 17)->get()->first();
+        # 2. delete the file from storage using FileFacades
+        FileFacades::delete(public_path($file->file_path));
+        # 3. delete the path from database
+        $file->delete();
+
         $files = File::all();
         return view("File.index", ["files" => $files]);
     }
