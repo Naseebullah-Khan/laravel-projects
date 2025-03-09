bd53a9416f9b1779204dd18f16cc4bff572e8c38
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-03-09 23:04:46 +0430

    Joins Inner Join

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("/join", function () {
+    $usersWithOrders = DB::table("users")
+        ->join("orders", "users.id", "=", "orders.user_id")
+        // ->select("users.*", "orders.*") # All columns of both tables
+        ->select("users.email", "orders.product_name") # only email column from user table and product_name column from orders table
+        ->get();
+
+    dd($usersWithOrders);
 });
