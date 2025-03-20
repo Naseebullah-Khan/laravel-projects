4daab1b6eb8f57df742a3feef854ffa1d95df7e0
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-03-20 22:15:41 +0430

    Middleware Global Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,22 @@
 <?php
 
+use App\Http\Middleware\CheckRoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(CheckRoleMiddleware::class);
+        // $middleware->append(AnotherMiddleware);
+        // $middleware->append(AnotherMiddleware);
+        // $middleware->append(AnotherMiddleware);
+        // you can add as many as you want
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
