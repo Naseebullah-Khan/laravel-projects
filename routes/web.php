6641bee8fab4eed853749d2a6cf77a1008d6a514
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-03-23 22:42:38 +0430

    Authentication Install Breeze Starter Kit

--- routes/web.php
+++ routes/web.php
@@ -1,129 +1,20 @@
 <?php
 
-use App\Http\Controllers\MiddlewareTestingController;
-use App\Models\Address;
-use App\Models\City;
-use App\Models\Country;
-use App\Models\Post;
-use App\Models\State;
-use App\Models\Tag;
-use App\Models\User;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Contracts\View\View;
 
-Route::get('/', function (): View {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/users", function (): View {
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    $users = User::all();
-    $addresses = Address::all();
-
-    return view("test", compact("users", "addresses"));
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get("/posts", function (): View {
-    // Post::insert([
-    //     [
-    //         "user_id" => 1,
-    //         "title" => "Learn Java",
-    //     ],
-    //     [
-    //         "user_id" => 5,
-    //         "title" => "Learn C++",
-    //     ],
-    //     [
-    //         "user_id" => 2,
-    //         "title" => "Learn C#",
-    //     ],
-    //     [
-    //         "user_id" => 1,
-    //         "title" => "Learn C",
-    //     ],
-    //     [
-    //         "user_id" => 5,
-    //         "title" => "Learn Python",
-    //     ],
-    // ]);
-
-    // Tag::insert([
-    //     [
-    //         "name" => ".NET"
-    //     ],
-    //     [
-    //         "name" => "Django"
-    //     ],
-    //     [
-    //         "name" => "Spring Boot"
-    //     ],
-    //     [
-    //         "name" => "C Framework"
-    //     ],
-    //     [
-    //         "name" => "C# Framework"
-    //     ],
-    // ]);
-
-    # add a tag to post using attach method
-    // $post = Post::find(5);
-    // $tag = Tag::find(1);
-    // $post->tags()->attach($tag);
-    # remove a tag from post using detach method
-    // $post->tags()->detach($tag);
-    # remove all tags and add new one or ones using sync method
-    // $post->tags()->sync($tag);
-
-    $posts = Post::all();
-    return view("posts", compact("posts"));
-});
-
-Route::get("/tags", function (): View {
-
-    $tags = Tag::all();
-
-    return view("tags", compact("tags"));
-});
-
-Route::get("/location", function (): View {
-
-    // $country = new Country(["name" => "Afghanistan"]);
-    // $country->save();
-
-    // $state = new State(["name" => "South"]);
-    // $country->states()->save($state);
-
-    // $kandaharCity = new City(["name" => "Kandahar"]);
-    // $helmandCity = new City(["name" => "Helmand"]);
-    // $state->cities()->saveMany([$kandaharCity, $helmandCity]);
-
-    $country = Country::first();
-
-    return view("location", compact("country"));
-});
-
-Route::get("/image", function () {
-
-    // $user = User::findOrFail(10);
-    // $user->image()->create(["path" => "/uploads/user_image.jpg"]);
-
-    // return $user->image;
-
-    $post = Post::findOrFail(4);
-    // $post->image()->create(["path" => "/uploads/post_image.jpg"]);
-
-    return $post->image;
-});
-
-// Route::get("/middlewareTesting", [MiddlewareTestingController::class, "index"])->name("middlewareTesting.index");
-// Route::post("/middlewareTesting", [MiddlewareTestingController::class, "store"])->name("middlewareTesting.store")
-//     ->middleware(["checkRole"]);
-
-# If you want to pass a parameter to the middleware, it is recommended to first alias to the middleware
-Route::get("/user/dashboard", function () {
-    dd("User Dashboard");
-})->middleware("checkRole:user"); // if you want to pass more than one parameters then you are gonna do it like this: "checkRole:admin,anotherParameter,anotherParameter"
-
-Route::get("/admin/dashboard", function () {
-    dd("Admin Dashboard");
-})->middleware("checkRole:admin"); // if you want to pass more than one parameters then you are gonna do it like this: "checkRole:admin,anotherParameter,anotherParameter"
+require __DIR__.'/auth.php';
