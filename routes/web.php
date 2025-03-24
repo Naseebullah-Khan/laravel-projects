9a4447fe9b175a78a96ca37f6bb70c855a58b4d3
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-03-24 22:10:54 +0430

    Authentication Redirect User To Specific Page

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get("/user/dashboard", function () {
+    dd("User Dashboard");
+})->name("user.dashboard");
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
