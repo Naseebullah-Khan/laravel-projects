1940d0d7fb88b1232542f7273c821a185e08bae4
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-08-02 20:33:09 +0430

    Mail Attaching files to email

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -51,6 +52,8 @@ class SendMail extends Mailable
      */
     public function attachments(): array
     {
-        return [];
+        return [
+            Attachment::fromPath(public_path("uploads/Screenshot 2025-06-02 161216.png"))->as('screenshot.png'),
+        ];
     }
 }
