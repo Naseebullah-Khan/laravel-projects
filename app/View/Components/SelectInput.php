42afbb0abc10fdb40eb9535b635357076bf8aedc
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-08-18 15:52:00 +0430

    Creating Migrations

--- app/View/Components/SelectInput.php
+++ app/View/Components/SelectInput.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class select-input extends Component
+class SelectInput extends Component
 {
     /**
      * Create a new component instance.
--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -23,6 +23,7 @@
                     <div class="form-group">
                         <x-input-label for="colors" value="Colors" />
                         <x-select-input name="colors">
+                            <option value="">Select Color</option>
                             <option value="red">Red</option>
                             <option value="blue">Blue</option>
                             <option value="green">Green</option>
