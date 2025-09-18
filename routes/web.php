ca03f91ef18a2bebdc4d36275152ebf07a1369f5
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-09-18 20:43:05 +0430

    Broadcasting - Working with Private Channel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Events\NewMessage;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
@@ -9,14 +10,24 @@ Route::get('/', function (): View {
     return view('welcome');
 });
 
-Route::get("messages", function (): View {
+Route::get('/dashboard', function (): View {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function (): void {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get("/messages", function (): View {
     return view("messages");
 });
 
-# in .env file if your QUEUE_CONNECTION is equal to database then you need to run the php artisan queue:work to broadcast the event
-# in .env file if your QUEUE_CONNECTION is equal to sync then you don't need to run the php artisan queue:work to broadcast the event
-Route::get("send-message", function (): RedirectResponse {
+Route::get("/send-message", function (): RedirectResponse {
     $message = request()->get("message");
     event(new NewMessage($message));
     return redirect("/messages");
 })->name("send-message");
+
+require __DIR__ . '/auth.php';
