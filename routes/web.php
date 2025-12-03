b8190eabd4ba614f2880ed42732dd58bd15100ba
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2025-12-03 15:15:05 +0430

    Artisan Console - Create a Laravel Command

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\CardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [CardController::class, "show"])->name("home");
+Route::get('/', function () {
+    return view('welcome');
+});
